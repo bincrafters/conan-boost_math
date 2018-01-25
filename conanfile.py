e92d6e235dd387c78175822aad419ced32fb43b1
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:26 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostMathConan(ConanFile):
     name = "boost_math"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_math"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["math"]
     is_in_cycle_group = True
     is_header_only = False
